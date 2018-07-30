646483fbbc474dd69ee6c6afcd66d9cfefdd8cf4
Michał Podskoczy <michal.podskoczy@gmail.com>
2018-07-30 23:23:37 +0200

    Debug output.

--- src/main/java/com/podskoczy/rx/RxPlaygroundApplication.java
+++ src/main/java/com/podskoczy/rx/RxPlaygroundApplication.java
@@ -31,15 +31,16 @@ public class RxPlaygroundApplication implements ApplicationRunner {
                 .flatMap(v ->
                         Flowable.just(v)
                                 .subscribeOn(Schedulers.io())
-                                .map(v1 -> {
-                                    return gitHubRepositoryDetails.getRepositoryDetails("allegro", "hermes");
-                                })
+                                .map(v1 -> gitHubRepositoryDetails.getRepositoryDetails("allegro", "hermes"))
+                                .doOnComplete(() -> System.out.println("Finished."))
+                                .doOnError(e -> System.out.println("Error during."))
                 ).blockingSubscribe();
         System.out.println(Duration.between(beforeParallel, LocalTime.now()).toMillis() + " ms.");
 
         LocalTime beforeSynchronized = LocalTime.now();
         for(int i=0; i<10; i++) {
             GitHubRepositoryDetailsModel repositoryDetailsModel = gitHubRepositoryDetails.getRepositoryDetails("allegro", "hermes");
+            System.out.println("Finished.");
         }
         System.out.println(Duration.between(beforeSynchronized, LocalTime.now()).toMillis() + "ms.");
     }

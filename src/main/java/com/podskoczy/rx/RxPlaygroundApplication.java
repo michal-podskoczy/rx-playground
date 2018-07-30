cb546fe18b59bdfc8a1cc30ece03b69cebdf4198
Michał Podskoczy <michal.podskoczy@gmail.com>
2018-07-30 21:49:01 +0200

    Init maven project with spring application.

---
+++ src/main/java/com/podskoczy/rx/RxPlaygroundApplication.java
@@ -0,0 +1,14 @@
+package com.podskoczy.rx;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxPlaygroundApplication  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RxPlaygroundApplication.class);
+    }
+
+}
